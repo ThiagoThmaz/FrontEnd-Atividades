720116562d051549d3e24910ba7f84087cba0ff5
ThiagoThmaz <thiago.conceicao@sptech.school>
2024-08-16 11:22:26 -0300

    Primeiro App

---
+++ primeiro-app/src/App.jsx
@@ -0,0 +1,20 @@
+import Componente1 from "./components/Componente1"
+
+function App() {
+  
+  const aluno = "João"
+  
+  return (
+   <>
+    <h1>Primeira Aula</h1>
+    <p>Nossa primeira Aula</p>
+    <p>O Aluno {aluno}</p>
+    <Componente1/>
+   </>
+  
+   
+  )
+
+}
+
+export default App
---
+++ primeiro-app/src/components/Componente1.jsx
@@ -0,0 +1,17 @@
+import logoreact from './../assets/react.svg'
+
+function Componente1(){
+
+
+    return(
+       <>
+        <h2>Componente1</h2>
+        <br />
+        <img src={logoreact} alt="Logo React" />
+        <img src="vite.svg" alt="Logo do Vite" />
+       </>
+    )
+
+}
+
+export default Componente1
