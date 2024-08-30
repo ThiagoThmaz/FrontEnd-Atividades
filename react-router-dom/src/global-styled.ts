4b2dd7cd4d0b430a8b14ea50388e9ad56d7304bf
ThiagoThmaz <thiago.conceicao@sptech.school>
2024-08-30 11:53:03 -0300

    React Router Dom

---
+++ react-router-dom/src/global-styled.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyled = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+
+    #root{
+        width: 100vw;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+
+    main{
+        flex-grow: 1;
+    }
+
+`
+
+export default GlobalStyled
---
+++ react-router-dom/src/styled.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const NavMenu = styled.nav`
+    width: 100%;
+    min-height: 8vh;
+    background-color: #fff;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid black;
+
+    a{
+        color: black;
+        text-decoration: none;
+        font-size: 1.3em;
+        padding: 2px;
+    }
+
+    span{
+        color: black;
+        font-size: 1.3em;
+    }
+
+`
+
